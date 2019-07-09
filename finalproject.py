ac00e843bf4a08735a087543210fcb68c1669f5d
Rodrigo Montebello Saboya Brito <blackknight@blackknightstudio.com.br>
2019-07-09 15:49:38 -0300

    Styled text and code.

--- finalproject.py
+++ finalproject.py
@@ -12,17 +12,17 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-# @app.route('/restaurant/<int:restaurant_id>/menu/JSON')
-# def restaurantMenuJSON(restaurant_id):
-#     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+# @app.route('/restaurant/<int:category_id>/menu/JSON')
+# def restaurantMenuJSON(category_id):
+#     restaurant = session.query(Restaurant).filter_by(id=category_id).one()
 #     items = session.query(MenuItem).filter_by(
-#         restaurant_id=restaurant_id).all()
+#         category_id=category_id).all()
 #     return jsonify(MenuItems=[i.serialize for i in items])
 #
 #
-# @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/JSON')
-# def menuItemJSON(restaurant_id, menu_id):
-#     Menu_Item = session.query(MenuItem).filter_by(id=menu_id).one()
+# @app.route('/restaurant/<int:category_id>/menu/<int:items_id>/JSON')
+# def menuItemJSON(category_id, items_id):
+#     Menu_Item = session.query(MenuItem).filter_by(id=items_id).one()
 #     return jsonify(Menu_Item=Menu_Item.serialize)
 #
 #
@@ -40,12 +40,12 @@ def showCategories():
     session = DBSession()
     categories = session.query(Categories).all()
     # return "This page will show all my restaurants"
-    return render_template('restaurants.html', categories = categories)
+    return render_template('home.html', categories = categories)
 
 
 # Create a new restaurant
 @app.route('/weaponsGuide/new/', methods=['GET', 'POST'])
-def newRestaurant():
+def newCategory():
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
     if request.method == 'POST':
@@ -54,47 +54,47 @@ def newRestaurant():
         session.commit()
         return redirect(url_for('showCategories'))
     else:
-        return render_template('newRestaurant.html')
+        return render_template('newCategory.html')
     # return "This page will be for making a new restaurant"
 
 # # Edit a restaurant
 #
 #
-@app.route('/weaponsGuide/<int:restaurant_id>/edit/', methods=['GET', 'POST'])
-def editCategory(restaurant_id):
+@app.route('/weaponsGuide/<int:category_id>/edit/', methods=['GET', 'POST'])
+def editCategory(category_id):
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
-    editedRestaurant = session.query(
-        Categories).filter_by(id=restaurant_id).one()
+    editedCategory = session.query(
+        Categories).filter_by(id=category_id).one()
     if request.method == 'POST':
         if request.form['name']:
-            editedRestaurant.name = request.form['name']
+            editedCategory.name = request.form['name']
             session.commit()
-            return redirect(url_for('showCat', category_id = restaurant_id ))
+            return redirect(url_for('showCat', category_id = category_id ))
     else:
         return render_template(
-            'editRestaurant.html', restaurant=editedRestaurant)
+            'editCategory.html', category=editedCategory)
 
-    # return 'This page will be for editing restaurant %s' % restaurant_id
+    # return 'This page will be for editing restaurant %s' % category_id
 #
 # # Delete a restaurant
 #
 #
-@app.route('/weaponsGuide/<int:restaurant_id>/delete/', methods=['GET', 'POST'])
-def deleteRestaurant(restaurant_id):
+@app.route('/weaponsGuide/<int:category_id>/delete/', methods=['GET', 'POST'])
+def deleteCategory(category_id):
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
-    restaurantToDelete = session.query(
-        Categories).filter_by(id=restaurant_id).one()
+    categoryToDelete = session.query(
+        Categories).filter_by(id=category_id).one()
     if request.method == 'POST':
-        session.delete(restaurantToDelete)
+        session.delete(categoryToDelete)
         session.commit()
         return redirect(
             url_for('showCategories'))
     else:
         return render_template(
-            'deleteRestaurant.html', restaurant=restaurantToDelete)
-#     # return 'This page will be for deleting restaurant %s' % restaurant_id
+            'deleteCategory.html', category=categoryToDelete)
+#     # return 'This page will be for deleting restaurant %s' % category_id
 #
 #
 # Show a restaurant menu
@@ -106,40 +106,40 @@ def showCat(category_id):
     categories = session.query(Categories).filter_by(id=category_id).one()
     items = session.query(CategoryItem).filter_by(
         categories_id=category_id).all()
-    return render_template('itens.html', items=items, category=categories)
-    # return 'This page is the menu for restaurant %s' % restaurant_id
+    return render_template('weapons.html', items=items, category=categories)
+    # return 'This page is the menu for restaurant %s' % category_id
 #
 # # Create a new menu item
 #
 #
 @app.route(
-    '/weaponsGuide/<int:restaurant_id>/menu/new/', methods=['GET', 'POST'])
-def newMenuItem(restaurant_id):
+    '/weaponsGuide/<int:category_id>/menu/new/', methods=['GET', 'POST'])
+def newWeapon(category_id):
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
     if request.method == 'POST':
         newItem = CategoryItem(name=request.form['name'], description=request.form[
-                           'description'], picture=request.form['picture'], categories_id=restaurant_id)
+                           'description'], picture=request.form['picture'], categories_id=category_id)
         session.add(newItem)
         session.commit()
 
-        return redirect(url_for('showCat', category_id=restaurant_id))
+        return redirect(url_for('showCat', category_id=category_id))
     else:
-        return render_template('newmenuitem.html', restaurant_id=restaurant_id)
+        return render_template('newWeapon.html', category_id=category_id)
 
-    return render_template('newMenuItem.html', restaurant_id=restaurant_id)
+    return render_template('newWeapon.html', category_id=category_id)
     # return 'This page is for making a new menu item for restaurant %s'
-    # %restaurant_id
+    # %category_id
 
 # # Edit a menu item
 #
 #
-@app.route('/weaponsGuide/<int:restaurant_id>/menu/<int:menu_id>/edit',
+@app.route('/weaponsGuide/<int:category_id>/menu/<int:items_id>/edit',
            methods=['GET', 'POST'])
-def editMenuItem(restaurant_id, menu_id):
+def editWeapon(category_id, items_id):
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
-    editedItem = session.query(CategoryItem).filter_by(id=menu_id).one()
+    editedItem = session.query(CategoryItem).filter_by(id=items_id).one()
     if request.method == 'POST':
         if request.form['name']:
             editedItem.name = request.form['name']
@@ -149,30 +149,30 @@ def editMenuItem(restaurant_id, menu_id):
             editedItem.price = request.form['picture']
         session.add(editedItem)
         session.commit()
-        return redirect(url_for('showCat', category_id=restaurant_id))
+        return redirect(url_for('showCat', category_id=category_id))
     else:
 
         return render_template(
-            'editmenuitem.html', restaurant_id=restaurant_id, menu_id=menu_id, item=editedItem)
+            'editWeapon.html', category_id=category_id, items_id=items_id, item=editedItem)
 #
-#     # return 'This page is for editing menu item %s' % menu_id
+#     # return 'This page is for editing menu item %s' % items_id
 #
 # # Delete a menu item
 #
 #
-@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete',
+@app.route('/restaurant/<int:category_id>/menu/<int:items_id>/delete',
            methods=['GET', 'POST'])
-def deleteMenuItem(restaurant_id, menu_id):
+def deleteWeapon(category_id, items_id):
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
-    itemToDelete = session.query(CategoryItem).filter_by(id=menu_id).one()
+    itemToDelete = session.query(CategoryItem).filter_by(id=items_id).one()
     if request.method == 'POST':
         session.delete(itemToDelete)
         session.commit()
-        return redirect(url_for('showCat', category_id=restaurant_id))
+        return redirect(url_for('showCat', category_id=category_id))
     else:
-        return render_template('deleteMenuItem.html', item=itemToDelete)
-#     # return "This page is for deleting menu item %s" % menu_id
+        return render_template('deleteWeapon.html', item=itemToDelete)
+#     # return "This page is for deleting menu item %s" % items_id
 
 
 if __name__ == '__main__':
